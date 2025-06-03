94483fb3fb806a7393d767523f3482306d8b68aa
httpsabhi <abhaykumar714371@gmail.com>
2025-06-03 15:00:57 +0530

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import Header from "../components/Header";
 import Footer from "../components/Footer";
 import SummaryCard from "../components/SummaryCard";
 import Button from "../components/Button";
